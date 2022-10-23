cd81298fcaff2d54421154a3be2dfd1d8ad1d504
Triangleyj <dpwns99@gmail.com>
2022-10-23 20:29:21 +0900

    Add mui design

--- src/Board.js
+++ src/Board.js
@@ -1,47 +1,74 @@
 // basic react functional component
 import './Board.css';
 import React from 'react';
+import { TextField, IconButton, Card, CardContent, List, ListItem, Divider, Typography, Button } from '@mui/material';
+import { Add, Delete } from '@mui/icons-material';
+
+// calculate between two times and return formatted string with mm:ss.ms
+const calculateTime = (start, end) => {
+    const neg = end < start;
+    const diff = Math.abs(end - start);
+    const ms = diff % 1000;
+    const s = Math.floor(diff / 1000) % 60;
+    const m = Math.floor(diff / 60000) % 60;
+    return `${neg ? '-' : ''}${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}.${ms.toString().padStart(3, '0')}`;
+}
+
 
 // expandable list jsx
 const ExpandableList = (props) => {
-    
+
     return (
-        <div className="expandable-list">
+        <ListItem className="expandable-list">
 
             <div className="expandable-list-header">
                 {/* <span className="expandable-list-header-index">
                     {props.index}
                 </span> */}
-                <input className="expandable-list-header-title"
-                    value={props.title ? props.title : 'New Task ' + props.index}
+                <TextField className="expandable-list-header-title"
+                    value={props.title}
+                    variant="standard"
+                    placeholder={'New Topic ' + props.index}
+                    varient="standard"
                     onInput={e => props.onUpdateTitle(e.target.value)} />
-                <span className="expandable-list-header-button">
-                    <button onClick={props.onAdd}>Add</button>
+                <span className="timer">
+                    {props.timer}
                 </span>
-                <span className="expandable-list-header-button">
-                    <button onClick={props.onDelete}>Delete</button>
-                </span>
+                <IconButton edge="end" aria-label="add" className="expandable-list-header-button" onClick={props.onAdd}>
+                    <Add>Add</Add>
+                </IconButton>
+                <IconButton edge="end" aria-label="delete" className="expandable-list-header-button" onClick={props.onDelete}>
+                    <Delete>Delete</Delete>
+                </IconButton>
             </div>
 
             <div className="expandable-list-body">
                 {props.children}
             </div>
 
-        </div>
+        </ListItem>
     );
 }
 
 // expandable list item jsx
 const ExpandableListItem = (props) => {
     return (
-        <div className="expandable-list-item">
-            <input className="expandable-list-item-title"
-                value={props.title ? props.title : 'New Item ' + props.index}
+        <ListItem className="expandable-list-item" sx={{ ml: 4}}>
+            <TextField className="expandable-list-item-title"
+                value={props.title}
+                placeholder={'New Task ' + props.index}
+                variant="standard"
                 onInput={e => props.onUpdateTitle(e.target.value)} />
-            <span className="expandable-list-item-button">
-                <button onClick={props.onDelete}>Delete</button>
-            </span>
-        </div>
+            <TextField className="expandable-list-item-title"
+                value={props.time}
+                placeholder={'Expect (s)'}
+                variant="standard"
+                sx={{ width: 100, ml: 2 }}
+                onInput={e => props.onUpdateTime(e.target.value)} />
+            <IconButton edge="end" aria-label="delete"  className="expandable-list-item-button" onClick={props.onDelete}>
+                <Delete>Delete</Delete>
+            </IconButton>
+        </ListItem>
     );
 }
 
@@ -51,33 +78,34 @@ class Board extends React.Component {
         super(props);
         this.state = {
             lists: [
-                
+
             ]
         };
     }
-    
+
     // add list
     addList = () => {
         const lists = this.state.lists;
         lists.push({
             title: '',
-            items: []
+            items: [],
+            open: true
         });
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
     }
 
     // delete list
     deleteList = (index) => {
         const lists = this.state.lists;
         lists.splice(index, 1);
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
     }
 
     // update list title
     updateListTitle = (index, title) => {
         const lists = this.state.lists;
         lists[index].title = title;
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
     }
 
     // add item
@@ -86,62 +114,77 @@ class Board extends React.Component {
         lists[index].items.push({
             title: ''
         });
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
     }
 
     // update item title
     updateItemTitle = (listIndex, itemIndex, title) => {
         const lists = this.state.lists;
         lists[listIndex].items[itemIndex].title = title;
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
+    }
+
+    // update item time
+    updateItemTime = (listIndex, itemIndex, time) => {
+        const lists = this.state.lists;
+        lists[listIndex].items[itemIndex].time = time;
+        this.setState({ lists: lists });
     }
 
     // delete item
     deleteItem = (listIndex, itemIndex) => {
         const lists = this.state.lists;
         lists[listIndex].items.splice(itemIndex, 1);
-        this.setState({lists: lists});
+        this.setState({ lists: lists });
     }
 
     // render
     render() {
         return (
-            <div className="board">
-                <div className="board-header">
-                    <span className="board-header-title">
-                        Board
-                    </span>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <div className="board-header">
+                        <Typography gutterBottom variant="h5" component="div">
+                            Board
+                        </Typography>
+                    </div>
+                    <List>
+                        <div className="board-body">
+                            {this.state.lists.map((list, listIndex) => {
+                                return (
+                                    <div>
+                                        <Divider />
+                                        <ExpandableList
+                                            key={listIndex}
+                                            index={listIndex}
+                                            title={list.title}
+                                            onAdd={() => this.addItem(listIndex)}
+                                            onDelete={() => this.deleteList(listIndex)}
+                                            onUpdateTitle={(title) => this.updateListTitle(listIndex, title)}
+                                        >
+                                        </ExpandableList>
+                                        {list.items.map((item, itemIndex) => {
+                                                return (
+                                                    <ExpandableListItem
+                                                        key={itemIndex}
+                                                        index={itemIndex}
+                                                        title={item.title}
+                                                        onDelete={() => this.deleteItem(listIndex, itemIndex)}
+                                                        onUpdateTitle={(title) => this.updateItemTitle(listIndex, itemIndex, title)}
+                                                        onUpdateTime={(title) => this.updateItemTime(listIndex, itemIndex, title)}
+                                                    />
+                                                );
+                                        })}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </List>
                     <span className="board-header-button">
-                        <button onClick={this.addList}>Add List</button>
+                        <Button onClick={this.addList}>Add Topic</Button>
                     </span>
-                </div>
-                <div className="board-body">
-                    {this.state.lists.map((list, listIndex) => {
-                        return (
-                            <ExpandableList
-                                key={listIndex}
-                                index={listIndex}
-                                title={list.title}
-                                onAdd={() => this.addItem(listIndex)}
-                                onDelete={() => this.deleteList(listIndex)}
-                                onUpdateTitle={(title) => this.updateListTitle(listIndex, title)}
-                                >
-                                {list.items.map((item, itemIndex) => {
-                                    return (
-                                        <ExpandableListItem
-                                        key={itemIndex}
-                                        index={itemIndex}
-                                        title={item.title}
-                                        onDelete={() => this.deleteItem(listIndex, itemIndex)}
-                                        onUpdateTitle={(title) => this.updateItemTitle(listIndex, itemIndex, title)}
-                                        />
-                                    );
-                                })}
-                            </ExpandableList>
-                        );
-                    })}
-                </div>
-            </div>
+                </CardContent>
+            </Card>
         );
     }
 
