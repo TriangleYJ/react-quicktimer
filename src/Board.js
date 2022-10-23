76bcea65e0604dad8ec6ac5ab3508749c02c8de3
Triangleyj <dpwns99@gmail.com>
2022-10-23 21:29:05 +0900

    Complete layout

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,8 @@
 // basic react functional component
 import './Board.css';
 import React from 'react';
-import { TextField, IconButton, Card, CardContent, List, ListItem, Divider, Typography, Button } from '@mui/material';
-import { Add, Delete } from '@mui/icons-material';
+import { Box, TextField, IconButton, Card, CardContent, List, ListItem, Divider, Typography, Button } from '@mui/material';
+import { Add, Delete, PlayArrow, Pause } from '@mui/icons-material';
 
 // calculate between two times and return formatted string with mm:ss.ms
 const calculateTime = (start, end) => {
@@ -19,56 +19,78 @@ const calculateTime = (start, end) => {
 const ExpandableList = (props) => {
 
     return (
-        <ListItem className="expandable-list">
-
-            <div className="expandable-list-header">
-                {/* <span className="expandable-list-header-index">
+        <Right2TextsWrapper text1="00:00.000" text2="00:00.000">
+            <ListItem className="expandable-list">
+                <Box>
+                    {/* <span className="expandable-list-header-index">
                     {props.index}
                 </span> */}
-                <TextField className="expandable-list-header-title"
-                    value={props.title}
-                    variant="standard"
-                    placeholder={'New Topic ' + props.index}
-                    varient="standard"
-                    onInput={e => props.onUpdateTitle(e.target.value)} />
-                <span className="timer">
-                    {props.timer}
-                </span>
-                <IconButton edge="end" aria-label="add" className="expandable-list-header-button" onClick={props.onAdd}>
-                    <Add>Add</Add>
-                </IconButton>
-                <IconButton edge="end" aria-label="delete" className="expandable-list-header-button" onClick={props.onDelete}>
-                    <Delete>Delete</Delete>
-                </IconButton>
-            </div>
+                    <TextField className="expandable-list-header-title"
+                        value={props.title}
+                        variant="standard"
+                        placeholder={'New Topic ' + props.index}
+                        varient="standard"
+                        onInput={e => props.onUpdateTitle(e.target.value)} />
+                    <span className="timer">
+                        {props.timer}
+                    </span>
+                    <IconButton edge="end" aria-label="add" className="expandable-list-header-button" onClick={props.onAdd}>
+                        <Add>Add</Add>
+                    </IconButton>
+                    <IconButton edge="end" aria-label="delete" className="expandable-list-header-button" onClick={props.onDelete}>
+                        <Delete>Delete</Delete>
+                    </IconButton>
+                </Box>
 
-            <div className="expandable-list-body">
-                {props.children}
-            </div>
-
-        </ListItem>
+                <div className="expandable-list-body">
+                    {props.children}
+                </div>
+            </ListItem>
+        </Right2TextsWrapper>
     );
 }
 
 // expandable list item jsx
 const ExpandableListItem = (props) => {
     return (
-        <ListItem className="expandable-list-item" sx={{ ml: 4}}>
-            <TextField className="expandable-list-item-title"
-                value={props.title}
-                placeholder={'New Task ' + props.index}
-                variant="standard"
-                onInput={e => props.onUpdateTitle(e.target.value)} />
-            <TextField className="expandable-list-item-title"
-                value={props.time}
-                placeholder={'Expect (s)'}
-                variant="standard"
-                sx={{ width: 100, ml: 2 }}
-                onInput={e => props.onUpdateTime(e.target.value)} />
-            <IconButton edge="end" aria-label="delete"  className="expandable-list-item-button" onClick={props.onDelete}>
-                <Delete>Delete</Delete>
-            </IconButton>
-        </ListItem>
+        <Right2TextsWrapper text1="00.00:000" text2="00.00:000">
+            <ListItem className="expandable-list-item">
+                <Box sx={{ ml: 4, display: 'flex' }}>
+                    <TextField className="expandable-list-item-title"
+                        value={props.title}
+                        placeholder={'New Task ' + props.index}
+                        variant="standard"
+                        onInput={e => props.onUpdateTitle(e.target.value)} />
+                    <TextField className="expandable-list-item-title"
+                        value={props.time}
+                        placeholder={'Expect (s)'}
+                        variant="standard"
+                        sx={{ width: 80, ml: 2 }}
+                        onInput={e => props.onUpdateTime(e.target.value)} />
+                    <IconButton onClick={()=>{}}>
+                        {props.counter ? <Pause>Pause</Pause> : <PlayArrow>Play</PlayArrow>}
+                    </IconButton>
+                    <IconButton onClick={props.onDelete}>
+                        <Delete>Delete</Delete>
+                    </IconButton>
+                </Box>
+            </ListItem>
+        </Right2TextsWrapper>
+    );
+}
+
+const Right2TextsWrapper = (props) => {
+    return (
+        <Box className="right2-texts-wrapper" sx={{ justifyContent: 'space-between', display: 'flex' }}>
+            <Box sx={{ alignItems: 'center', display: 'flex' }}>
+                {props.children}
+            </Box>
+
+            <Box sx={{ alignItems: 'center', display: 'flex' }}>
+                <Typography sx={{ m: 1, fontWeight: props.bold ? 'bold' : 'regular', width: 80 }}>{props.text1}</Typography>
+                <Typography sx={{ m: 1, fontWeight: props.bold ? 'bold' : 'regular', width: 80 }}>{props.text2}</Typography>
+            </Box>
+        </Box>
     );
 }
 
@@ -96,9 +118,11 @@ class Board extends React.Component {
 
     // delete list
     deleteList = (index) => {
-        const lists = this.state.lists;
-        lists.splice(index, 1);
-        this.setState({ lists: lists });
+        if(window.confirm('Are you sure you want to delete this list?')){
+            const lists = this.state.lists;
+            lists.splice(index, 1);
+            this.setState({ lists: lists });
+        }
     }
 
     // update list title
@@ -133,9 +157,11 @@ class Board extends React.Component {
 
     // delete item
     deleteItem = (listIndex, itemIndex) => {
-        const lists = this.state.lists;
-        lists[listIndex].items.splice(itemIndex, 1);
-        this.setState({ lists: lists });
+        if(window.confirm('Are you sure you want to delete this item?')){
+            const lists = this.state.lists;
+            lists[listIndex].items.splice(itemIndex, 1);
+            this.setState({ lists: lists });
+        }
     }
 
     // render
@@ -144,9 +170,16 @@ class Board extends React.Component {
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
                     <div className="board-header">
-                        <Typography gutterBottom variant="h5" component="div">
-                            Board
-                        </Typography>
+                        <Right2TextsWrapper text1="Goal" text2="Diff" bold>
+                            <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 'bold'}}>
+                                Record
+                            </Typography>
+                        </Right2TextsWrapper>
+                        <Right2TextsWrapper text1="00:00.000" text2="00:00.000">
+                            <Typography>
+                                Total Time
+                            </Typography>
+                        </Right2TextsWrapper>
                     </div>
                     <List>
                         <div className="board-body">
@@ -164,16 +197,16 @@ class Board extends React.Component {
                                         >
                                         </ExpandableList>
                                         {list.items.map((item, itemIndex) => {
-                                                return (
-                                                    <ExpandableListItem
-                                                        key={itemIndex}
-                                                        index={itemIndex}
-                                                        title={item.title}
-                                                        onDelete={() => this.deleteItem(listIndex, itemIndex)}
-                                                        onUpdateTitle={(title) => this.updateItemTitle(listIndex, itemIndex, title)}
-                                                        onUpdateTime={(title) => this.updateItemTime(listIndex, itemIndex, title)}
-                                                    />
-                                                );
+                                            return (
+                                                <ExpandableListItem
+                                                    key={itemIndex}
+                                                    index={itemIndex}
+                                                    title={item.title}
+                                                    onDelete={() => this.deleteItem(listIndex, itemIndex)}
+                                                    onUpdateTitle={(title) => this.updateItemTitle(listIndex, itemIndex, title)}
+                                                    onUpdateTime={(title) => this.updateItemTime(listIndex, itemIndex, title)}
+                                                />
+                                            );
                                         })}
                                     </div>
                                 );
